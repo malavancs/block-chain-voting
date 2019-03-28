724f248ffa24cce0b61185fcc90bb78b9bfcccec
Predator-PC <cubanhog.royalagamma@gmai.com>
2019-03-28 18:47:23 +0530

    balance ui changed

--- src/app/blockchain/etherum-pay/etherum-pay.component.ts
+++ src/app/blockchain/etherum-pay/etherum-pay.component.ts
@@ -6,10 +6,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./etherum-pay.component.css']
 })
 export class EtherumPayComponent implements OnInit {
+  cars: { sno: number; etherum_address: string; transaction_address: string; }[];
+  electionList: { label: string; value: string; }[];
 
   constructor() { }
 
   ngOnInit() {
+    this.electionList = [
+      {
+        label: 'Select an election to add',
+        value: null
+      },
+      {
+        label: '2019 Indian Election',
+        value: 'election_id'
+      }];
+
+      this.cars = [{
+        sno: 1,
+        etherum_address: 'sfjfakjnkjasndfasnfjasndfkjasndf',
+        transaction_address: 'poeddhsdfuwibfakhdbf-fasdfsf'
+      },{
+        sno: 1,
+        etherum_address: 'sfjfakjnkjasndfasnfjasndfkjasndf',
+        transaction_address: 'poeddhsdfuwibfakhdbf-fasdfsf'
+      },
+      {
+        sno: 1,
+        etherum_address: 'sfjfakjnkjasndfasnfjasndfkjasndf',
+        transaction_address: 'poeddhsdfuwibfakhdbf-fasdfsf'
+      }];
   }
 
 }
